51c47df1d5688e8a4afe39237ba072625ec87249
Mario Castro <mariocaster@gmail.com>
2015-11-15 13:04:05 +0100

    Some tests finished

--- public/views/articles/components/ListItem.jsx
+++ public/views/articles/components/ListItem.jsx
@@ -6,7 +6,7 @@ module.exports = React.createClass({
   render: function render(){
     return(
       <div>
-        <h4>{this.props.title}</h4>
+        <h4>{this.props.article.title}</h4>
       </div>
     );
   }
