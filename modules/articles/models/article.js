b1072c5bc4273eedfc7e683dd42bdbdac0ee4d1f
Mario Castro <mariocaster@gmail.com>
2015-11-21 20:31:22 +0100

    Article model and persistence functions

---
+++ modules/articles/models/article.js
@@ -0,0 +1,139 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var mongoose = require('mongoose'),
+  Schema = mongoose.Schema;
+
+/**
+ * Article Schema
+ */
+var ArticleSchema = new Schema({
+  created: {
+    type: Date,
+    default: Date.now
+  },
+  title: {
+    type: String,
+    default: '',
+    trim: true,
+    required: 'Title cannot be blank'
+  },
+  content: {
+    type: String,
+    default: '',
+    trim: true
+  },
+  user: {
+    type: Schema.ObjectId,
+    ref: 'User'
+  }
+});
+
+var Article = mongoose.model('Article', ArticleSchema);
+
+module.exports = Article;
+
+
+/**
+ * Create a article
+ */
+module.exports.create = function (req, res) {
+  var article = new Article(req.body);
+  article.user = req.user;
+
+  article.save(function (err) {
+    if (err) {
+      return res.status(400).send({
+        message: errorHandler.getErrorMessage(err)
+      });
+    } else {
+      res.json(article);
+    }
+  });
+};
+
+/**
+ * Show the current article
+ */
+module.exports.read = function (req, res) {
+  res.json(req.article);
+};
+
+/**
+ * Update a article
+ */
+module.exports.update = function (req, res) {
+  var article = req.article;
+
+  article.title = req.body.title;
+  article.content = req.body.content;
+
+  article.save(function (err) {
+    if (err) {
+      return res.status(400).send({
+        message: errorHandler.getErrorMessage(err)
+      });
+    } else {
+      res.json(article);
+    }
+  });
+};
+
+/**
+ * Delete an article
+ */
+module.exports.delete = function (req, res) {
+  var article = req.article;
+
+  article.remove(function (err) {
+    if (err) {
+      return res.status(400).send({
+        message: errorHandler.getErrorMessage(err)
+      });
+    } else {
+      res.json(article);
+    }
+  });
+};
+
+/**
+ * List of Articles
+ */
+module.exports.list = function (req, res) {
+  Article.find().sort('-created').populate('user', 'displayName').exec(function (err, articles) {
+    if (err) {
+      return res.status(400).send({
+        message: errorHandler.getErrorMessage(err)
+      });
+    } else {
+      res.json(articles);
+    }
+  });
+};
+
+/**
+ * Article middleware
+ */
+module.exports.articleByID = function (req, res, next, id) {
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).send({
+      message: 'Article is invalid'
+    });
+  }
+
+  Article.findById(id).populate('user', 'displayName').exec(function (err, article) {
+    if (err) {
+      return next(err);
+    } else if (!article) {
+      return res.status(404).send({
+        message: 'No article with that identifier has been found'
+      });
+    }
+    req.article = article;
+    next();
+  });
+};
+
